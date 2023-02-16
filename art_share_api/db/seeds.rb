d31f9ade40946b7ad81ac70408c05076fd9cbb2d
Justin <jcdiner@gmail.com>
2023-02-16 15:43:49 -0500

    seed data

--- art_share_api/db/seeds.rb
+++ art_share_api/db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+	{name: "Jill"}, 
+	{name: "Justin"}, 
+	{name: "Rod"}, 
+	{name: "Allison"}, 
+	{name: "Rico"}])
+
+artwork = Artwork.create([
+	{
+		title: "Mona Lisa",
+		img_url: "monalisa.com",
+		artist_id: 1
+	},
+	{
+		title: "Mona Pizza",
+		img_url: "moremonalisapizza.com",
+		artist_id: 2
+	},
+	{
+		title: "Hot Rod",
+		img_url: "poolsaregreat.com",
+		artist_id: 3
+	},
+	{
+		title: "TeaPot",
+		img_url: "418error.com",
+		artist_id: 2
+	},
+	{
+		title: "Kahoot",
+		img_url: "Kahoot.io",
+		artist_id: 1
+	}
+])
+
+artwork_shares = ArtworkShare.create([
+	{
+		artwork_id: 1,
+		viewer_id: 2
+	},
+	{
+		artwork_id: 3,
+		viewer_id: 3
+	},
+	{
+		artwork_id: 4,
+		viewer_id: 1
+	},
+	{
+		artwork_id: 4,
+		viewer_id: 2
+	}
+])
